c12b7b8a4407c91c7aecc4f229bb88a841697795
CKMatsika <clemie90@gmail.com>
2025-03-15 08:54:57 +0200

    Create Entry.cs

---
+++ week02/Journal/Entry.cs
@@ -0,0 +1,14 @@
+// Entry.cs
+using System;
+
+public class Entry
+{
+    public string _date;
+    public string _promptText;
+    public string _entryText;
+
+    public void Display()
+    {
+        // This will be implemented later
+    }
+}
