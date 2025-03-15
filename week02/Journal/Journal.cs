269da9167b354f7e5058851b2f2a23dc6eb3c1b6
CKMatsika <clemie90@gmail.com>
2025-03-15 08:55:26 +0200

    Create Journal.cs

---
+++ week02/Journal/Journal.cs
@@ -0,0 +1,29 @@
+// Journal.cs
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class Journal
+{
+    public List<Entry> _entries = new List<Entry>();
+
+    public void AddEntry(Entry newEntry)
+    {
+        // This will be implemented later
+    }
+
+    public void DisplayAll()
+    {
+        // This will be implemented later
+    }
+
+    public void SaveToFile(string filename)
+    {
+        // This will be implemented later
+    }
+
+    public void LoadFromFile(string filename)
+    {
+        // This will be implemented later
+    }
+}
