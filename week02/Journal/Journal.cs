31d9d8130946fb78106ada295f0f48c114724412
CKMatsika <clemie90@gmail.com>
2025-03-15 09:07:48 +0200

    Update Journal.cs

--- week02/Journal/Journal.cs
+++ week02/Journal/Journal.cs
@@ -1,29 +1,46 @@
-// Journal.cs
-using System;
-using System.Collections.Generic;
-using System.IO;
-
 public class Journal
 {
     public List<Entry> _entries = new List<Entry>();
-
+    
     public void AddEntry(Entry newEntry)
     {
-        // This will be implemented later
+        _entries.Add(newEntry);
     }
-
+    
     public void DisplayAll()
     {
-        // This will be implemented later
+        foreach (Entry entry in _entries)
+        {
+            entry.Display();
+        }
     }
-
+    
     public void SaveToFile(string filename)
     {
-        // This will be implemented later
+        using (StreamWriter outputFile = new StreamWriter(filename))
+        {
+            foreach (Entry entry in _entries)
+            {
+                outputFile.WriteLine($"{entry._date}~|~{entry._promptText}~|~{entry._entryText}");
+            }
+        }
     }
-
+    
     public void LoadFromFile(string filename)
     {
-        // This will be implemented later
+        _entries.Clear();
+        string[] lines = System.IO.File.ReadAllLines(filename);
+        
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split("~|~");
+            
+            Entry entry = new Entry();
+            entry._date = parts[0];
+            entry._promptText = parts[1];
+            entry._entryText = parts[2];
+            
+            _entries.Add(entry);
+        }
     }
 }
